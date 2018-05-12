75374c94c2f2821b0b7355bfeac5fed4e4931a7d
yxy2829 <yxy2829@gmail.com>
2018-05-12 15:42:12 +0800

    fix

--- weibo/pipelines.py
+++ weibo/pipelines.py
@@ -32,7 +32,7 @@ class WeiboPipeline():
             date = time.strftime('%Y-%m-%d %H:%M', time.localtime(time.time() - float(hour) * 60 * 60))
         if re.match('昨天.*', date):
             date = re.match('昨天(.*)', date).group(1).strip()
-            date = time.strftime('%Y-%m-%d', time.localtime() - 24 * 60 * 60) + ' ' + date
+            date = time.strftime('%Y-%m-%d', time.localtime(time.time() - 24 * 60 * 60)) + ' ' + date
         if re.match('\d{2}-\d{2}', date):
             date = time.strftime('%Y-', time.localtime()) + date + ' 00:00'
         return date
